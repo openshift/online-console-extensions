456ae8d34fab8af6545e40e2b81530fe8a9cd1be
Will Gordon <wgordon@redhat.com>
2017-09-28 15:27:50 -0400

    about: Update to include router information

--- ui/assets/extensions/online-extensions.js
+++ ui/assets/extensions/online-extensions.js
@@ -35,6 +35,7 @@
   window.OPENSHIFT_CONSTANTS.HELP["selector_label"] = window.OPENSHIFT_EXTENSION_PROPERTIES.doc_url+"dev_guide/selector_label_volume_binding.html";
   window.OPENSHIFT_CONSTANTS.HELP["notifications"] = window.OPENSHIFT_EXTENSION_PROPERTIES.doc_url+"dev_guide/notifications.html";
   window.OPENSHIFT_CONSTANTS.HELP["deployment-operations"] = window.OPENSHIFT_EXTENSION_PROPERTIES.doc_url+"dev_guide/deployments/basic_deployment_operations.html#start-deployment";
+  window.OPENSHIFT_CONSTANTS.HELP["creating_routes"] = window.OPENSHIFT_EXTENSION_PROPERTIES.doc_url + "dev_guide/routes.html#creating-routes";
 
   if (window.OPENSHIFT_EXTENSION_PROPERTIES.enable_pipelines) {
     window.OPENSHIFT_CONSTANTS.ENABLE_TECH_PREVIEW_FEATURE.pipelines = true;
@@ -82,7 +83,13 @@
       if (window.OPENSHIFT_EXTENSION_PROPERTIES.online_version) {
         $rootScope.online_version = "(online version " + window.OPENSHIFT_EXTENSION_PROPERTIES.online_version + ")";
       }
-      
+      if(window.OPENSHIFT_EXTENSION_PROPERTIES.default_route_suffix) {
+        $rootScope.default_route_suffix = window.OPENSHIFT_EXTENSION_PROPERTIES.default_route_suffix;
+      }
+      if(window.OPENSHIFT_EXTENSION_PROPERTIES.custom_routes_enabled) {
+        $rootScope.custom_routes_enabled = window.OPENSHIFT_EXTENSION_PROPERTIES.custom_routes_enabled;
+      }
+
       /*
        * Request system status from statuspage.io
        */
