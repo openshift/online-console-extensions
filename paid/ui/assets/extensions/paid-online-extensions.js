8c1628acebd8a5e160bdde6da6df324b7bccb403
Mike Dame <mdame@redhat.com>
2018-07-12 13:46:25 -0400

    Add free and paid online-extensions.js files for custom quota messages

---
+++ paid/ui/assets/extensions/paid-online-extensions.js
@@ -0,0 +1,10 @@
+/*
+ * Online extensions specific to pro tier
+ */
+
+window.OPENSHIFT_CONSTANTS.QUOTA_NOTIFICATION_MESSAGE = {
+  "memory": "<a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"\">Manage your subscription</a> to add additional resources",
+  "cpu": "<a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"\">Manage your subscription</a> to add additional resources",
+  "persistentvolumeclaims": "<a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"\">Manage your subscription</a> to add additional resources",
+  "requests.storage": "<a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"\">Manage your subscription</a> to add additional resources"
+}
