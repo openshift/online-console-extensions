3c4afa08555592d6563b4e23e212b8c4b65b9f41
Mike Dame <mdame@redhat.com>
2018-08-16 16:29:05 -0400

    Remove PVC quota message from paid and storage message from free

--- free/ui/assets/extensions/free-online-extensions.js
+++ free/ui/assets/extensions/free-online-extensions.js
@@ -5,6 +5,5 @@
 window.OPENSHIFT_CONSTANTS.QUOTA_NOTIFICATION_MESSAGE = {
   "memory": "Upgrade to <a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"/register/plan\">OpenShift Online Pro</a> for access to additional resources",
   "cpu": "Upgrade to <a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"/register/plan\">OpenShift Online Pro</a> for access to additional resources",
-  "persistentvolumeclaims": "Upgrade to <a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"/register/plan\">OpenShift Online Pro</a> for access to additional resources",
-  "requests.storage": "Upgrade to <a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"/register/plan\">OpenShift Online Pro</a> for access to additional resources"
+  "persistentvolumeclaims": "Upgrade to <a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"/register/plan\">OpenShift Online Pro</a> for access to additional resources"
 }
--- paid/ui/assets/extensions/paid-online-extensions.js
+++ paid/ui/assets/extensions/paid-online-extensions.js
@@ -5,6 +5,5 @@
 window.OPENSHIFT_CONSTANTS.QUOTA_NOTIFICATION_MESSAGE = {
   "memory": "<a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"\">Manage your subscription</a> to add additional resources",
   "cpu": "<a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"\">Manage your subscription</a> to add additional resources",
-  "persistentvolumeclaims": "<a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"\">Manage your subscription</a> to add additional resources",
   "requests.storage": "<a href=\""+window.OPENSHIFT_EXTENSION_PROPERTIES.account_url+"\">Manage your subscription</a> to add additional resources"
 }
